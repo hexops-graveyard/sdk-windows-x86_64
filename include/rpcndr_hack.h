13dcda7fe3f1aec0fc6130527226ad7ae0f4b792
Stephen Gutekanst <stephen@hexops.com>
2022-08-06 20:14:37 -0700

    reduce rpcndr_hack (defines no longer needed for Dawn compilation)
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- include/rpcndr_hack.h
+++ include/rpcndr_hack.h
@@ -11,15 +11,8 @@
 #define _Ret_notnull_
 #define _Inout_
 #define _Outptr_opt_
-#define _In_range_(lb, ub)
-#define _Out_writes_z_(size)
 #define _Use_decl_annotations_
-#define _Out_writes_(size)
-#define _In_opt_count_(size)
 #define _Outptr_result_maybenull_
 #define _Analysis_assume_(expr)
-#define _Field_size_full_(size)
-#define _In_reads_bytes_(size)
-#define _Field_size_opt_(size)
 #define _In_NLS_string_(size)
 #define _Outptr_
